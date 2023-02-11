a69f7fa1cd3696a3a0f1f254b7a8352703468809
radosan <radoslaw.sanetra@gmail.com>
2023-02-11 17:04:43 +0100

    checking arrival times

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -10,7 +10,9 @@ class BStop:
     def __init__(self, data):
         self.id = data['stop_id']
         self.name = data['stop_name']
+        self.next = data['next_stop']
         self.type = data['stop_type']
+        self.arrival = data['a_time']
 
 
 class EasyRider:
@@ -26,6 +28,7 @@ class EasyRider:
         self.field_errors = Counter()
         self.lines = {}
         self.s_stops = {'Start stops': set(), 'Transfer stops': set(), 'Finish stops': set()}
+        self.arr_err = {}
 
     def check_data(self):
         for dataset in self.data:
@@ -49,6 +52,20 @@ class EasyRider:
         for l1, l2 in itertools.combinations(self.lines.values(), 2):
             self.s_stops['Transfer stops'].update(set(x.name for x in l1) & set(x.name for x in l2))
 
+    def check_arrivals(self):
+        for line, stops in self.lines.items():
+            stops_sorted = [stop for stop in stops if stop.type == 'S']
+            next_s = stops_sorted[0].next
+            for _ in range(len(stops)-1):
+                stops_sorted.extend([stop for stop in stops if stop.id == next_s])
+                next_s = stops_sorted[-1].next
+            prev_time = '00:00'
+            for stop in stops_sorted:
+                if stop.arrival <= prev_time:
+                    self.arr_err[line] = stop.name
+                    break
+                prev_time = stop.arrival
+
     def disp_info(self, *args):
         if 'errors' in args:
             print(f'\nValidation: {self.field_errors.total()} errors')
@@ -62,6 +79,13 @@ class EasyRider:
             print()
             for key, value in self.s_stops.items():
                 print(key + ':', len(value), sorted(list(value)))
+        if 'arrivals' in args:
+            print('\nArrival time test:')
+            if not self.arr_err:
+                print('OK')
+            else:
+                for line, stop in self.arr_err.items():
+                    print(f'bus_id line {line}: wrong time on station {stop}')
 
 
 def main():
@@ -69,7 +93,8 @@ def main():
     rider = EasyRider(data)
     rider.check_data()
     rider.check_lines()
-    rider.disp_info('stops')
+    rider.check_arrivals()
+    rider.disp_info('arrivals')
 
 
 if __name__ == '__main__':
